56f7d2a8fe49552ba8f590214b44e72a63b68dec
ginpei <ginpei@gmail.com>
2017-12-23 23:09:48 -0800

    show time in digital

---
+++ src/screens/home/Clock.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+	div.Clock
+		p {{hours}}:{{minutes | digi2}}:{{seconds}}
+</template>
+
+<style lang="sass" scoped>
+	.Clock
+		border: 1px solid tomato
+</style>
+
+<script>
+	module.exports = {
+		props: [
+			'time',
+		],
+
+		computed: {
+			available(){
+				return Boolean(this.time)
+			},
+			hours(){
+				return this.time && this.time.getHours()
+			},
+			minutes(){
+				return this.time && this.time.getMinutes()
+			},
+			seconds(){
+				return this.time && this.time.getSeconds()
+			},
+		},
+
+		filters: {
+			/**
+			 * Make the number in 2 digits.
+			 * @param {number} n 0 to 99.
+			 * @returns {string}
+			 */
+			digi2(n) {
+				if (typeof n === 'number' && !isNaN(n)) {
+					return n < 10 ? '0' + n : n.toString()
+				} else {
+					return ''
+				}
+			},
+		},
+	}
+</script>
--- src/screens/home/index.vue
+++ src/screens/home/index.vue
@@ -4,6 +4,8 @@
 		ul
 			li
 				router-link(to="/settings") Settings
+
+		Clock(:time="time")
 </template>
 
 <style lang="sass" scoped>
@@ -11,18 +13,31 @@
 
 <script>
 	const Wrapper = require('../../components/base-layout/index.vue')
+	const Clock = require('./Clock.vue')
 
 	module.exports = {
 		components: {
 			Wrapper,
+			Clock,
 		},
 
 		data() {
 			return {
+				time: null,
 			}
 		},
 
 		computed: {
+		},
+
+		mounted() {
+			this.tmUpdateTime = setInterval(() => {
+				this.time = new Date()
+			}, 100)
+		},
+
+		destroyed() {
+			clearInterval(this.tmUpdateTime)
 		},
 
 		methods: {

8032ff2f1ccc686c0263b5544790f42bc47734c1
ginpei <ginpei@gmail.com>
2017-12-26 04:38:48 -0800

    generalize

--- script/server.js
+++ script/server.js
@@ -3,7 +3,7 @@ const app = express()
 
 const PORT = 3000
 
-app.use('/assets/app.js', express.static('./build/app.js'))
+app.use('/assets/', express.static('./build'))
 app.use('/', express.static('./static'))
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}.`))

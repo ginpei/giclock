2464d299a6b79df4b67c4984043134d18f52db26
ginpei <ginpei@gmail.com>
2018-01-12 15:48:03 -0800

    remove all

--- script/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-
-const PORT = 3000
-
-app.use('/assets/', express.static('./build'))
-app.use('/', express.static('./static'))
-
-app.listen(PORT, () => console.log(`Listening on port ${PORT}.`))
--- vue.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	sass: {
-		indentedSyntax: true,
-	},
-};

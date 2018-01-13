a191ed0e19160e97e4a2fe96f1afd0a04a6dad6f
ginpei <ginpei@gmail.com>
2018-01-12 16:50:42 -0800

    first digital clock

---
+++ components/DigitalClock.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="DigitalClock">
+    <span class="hours">{{sHours}}</span>
+    <span class="delimiter">:</span>
+    <span class="minutes">{{sMinutes}}</span>
+    <span class="delimiter">:</span>
+    <span class="seconds">{{sSeconds}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      now: new Date()
+    }
+  },
+
+  computed: {
+    sHours () {
+      const n = this.now.getHours()
+      const s = n < 10 ? `0${n}` : String(n)
+      return s
+    },
+
+    sMinutes () {
+      const n = this.now.getMinutes()
+      const s = n < 10 ? `0${n}` : String(n)
+      return s
+    },
+
+    sSeconds () {
+      const n = this.now.getSeconds()
+      const s = n < 10 ? `0${n}` : String(n)
+      return s
+    }
+  },
+
+  mounted () {
+    this.tmTick = setInterval(() => {
+      this.now = new Date()
+    }, 100)
+  },
+
+  destroyed () {
+    console.log('destroyed')
+    clearInterval(this.tmTick)
+  }
+}
+</script>
+
+<style>
+.DigitalClock {
+  font-family: monospace;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,15 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        giclock
-      </h1>
-      <h2 class="subtitle">
-        Clock
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <DigitalClock />
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import DigitalClock from '~/components/DigitalClock.vue'
 
 export default {
   components: {
-    Logo
+    DigitalClock
   }
 }
 </script>
@@ -33,26 +21,5 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>

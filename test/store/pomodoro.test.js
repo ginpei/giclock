4d96814f07f1e06331cf2b77e03e6b3f20a3c648
ginpei <ginpei@gmail.com>
2018-01-21 23:22:18 -0800

    test

---
+++ test/store/pomodoro.test.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai'
+import { mutations } from '../../store/pomodoro.js'
+
+describe('pomodoro', () => {
+  describe('mutations', () => {
+    it('is OK', () => {
+      expect(true).to.eql(true)
+    })
+  })
+})

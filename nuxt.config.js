19efa080af1b12e0887c34d4d6f3372b846ed52d
ginpei <ginpei@gmail.com>
2018-02-05 11:38:58 -0800

    default title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'giclock',
+    title: 'Clock',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },

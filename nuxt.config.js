e1cdd23fe96292903ce7dd00531e165c8c5cb95f
ginpei <ginpei@gmail.com>
2018-02-11 21:59:04 -0800

    icon as favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ module.exports = {
     link: [
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Share+Tech+Mono' },
       { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.0.4/css/all.css' },
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', href: '/icon512.png' },
     ],
   },
   /*
